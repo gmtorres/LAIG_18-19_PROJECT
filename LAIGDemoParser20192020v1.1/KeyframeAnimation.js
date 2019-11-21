524f23ae1954d4dfc2f5bf1da152d92e914526d1
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2019-11-21 12:52:28 +0000

    Some documentation

--- LAIGDemoParser20192020v1.1/KeyframeAnimation.js
+++ LAIGDemoParser20192020v1.1/KeyframeAnimation.js
@@ -1,5 +1,12 @@
 class KeyframeAnimation extends Animation{
-
+    /**
+     * 
+     * @param {CGFscene} scene Reference to MyScene
+     * @param {float} inst Time instance where the Key frame begins
+     * @param {vec3} trans Translation transformation
+     * @param {vec3} rota  Rotation transformation
+     * @param {vec3} scl   Scale transformation
+     */
     constructor(scene ,inst, trans, rota , scl){
         super(scene);
         this.instant = inst;
